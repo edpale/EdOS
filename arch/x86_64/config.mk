61d6442c45007fe3a77b06eab8cc7d3480fad918
Edgar Pale <ed@edpale.com>
2026-06-30 03:14:49 +0400

    Add -boot d flag to qemu
    Signed-off-by: Edgar Pale <ed@edpale.com>

--- arch/x86_64/config.mk
+++ arch/x86_64/config.mk
@@ -2,4 +2,4 @@ CC      = x86_64-elf-gcc
 AS      = x86_64-elf-as
 LD      = x86_64-elf-ld
 
-EMULATOR := qemu-system-x86_64 -cdrom $(FINAL_ISO) -serial stdio -no-reboot
+EMULATOR := qemu-system-x86_64 -cdrom $(FINAL_ISO) -serial stdio -no-reboot -boot d
