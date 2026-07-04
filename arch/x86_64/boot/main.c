1abd5b63a29c0d9623af0eaee01fc50e03f980a1
Edgar Pale <ed@edpale.com>
2026-07-04 23:08:13 +0400

    Wire in physmem
    Signed-off-by: Edgar Pale <ed@edpale.com>

--- arch/x86_64/boot/main.c
+++ arch/x86_64/boot/main.c
@@ -1,4 +1,6 @@
+#include <arch/pmap.h>
 #include <kernel/multiboot2.h>
+#include <kernel/physmem.h>
 #include <kernel/printf.h>
 #include <kernel/serial.h>
 
@@ -13,4 +15,6 @@ void kmain(u32 magic, paddr_t mbi_addr)
 	gdt_init();
 	serial_init();
 	mb2_process_info(mbi_addr);
+	physmem_exclude_region(kern_va_to_pa(_kernel_start),
+			       kern_va_to_pa(_kernel_end));
 }
--- kernel/multiboot2.c
+++ kernel/multiboot2.c
@@ -2,6 +2,7 @@
 #include <kernel/multiboot2.h>
 #include <kernel/printf.h>
 #include <kernel/utils.h>
+#include <kernel/physmem.h>
 
 static inline struct mb2_tag_header *mb2_next_tag(struct mb2_tag_header *tag)
 {
@@ -15,6 +16,8 @@ static void mb2_handle_mmap(struct mb2_tag_mmap *tag)
 	     i = (struct mb2_mmap_entry *)((u8 *)i + tag->entry_size)) {
 		kprintf("Mem area:\nstart: %llx\nlen: %llx\ntype %x\n\n",
 			i->addr, i->len, i->type);
+		if (i->type == MB2_MEMORY_AVAILABLE)
+			physmem_add_region(i->addr, i->addr + i->len);
 	}
 }
 
@@ -23,15 +26,15 @@ void mb2_process_info(paddr_t mbi_paddr)
 	struct mb2_info *info = boot_pa_to_va(mbi_paddr);
 
 	for (struct mb2_tag_header *i = info->tags;
-	     (u8 *)i - (u8 *)info < info->total_size;
+	     (u8 *)i - (u8 *)info < info->total_size &&
+	     i->type != MB2_TAG_TYPE_END;
 	     i = mb2_next_tag(i)) {
 		switch (i->type) {
 		case MB2_TAG_TYPE_MMAP:
 			mb2_handle_mmap((struct mb2_tag_mmap *)i);
 			break;
-		case MB2_TAG_TYPE_END:
-			goto end;
 		}
 	}
-end:
+
+	physmem_exclude_region(mbi_paddr, mbi_paddr + info->total_size);
 }
